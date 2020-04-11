6350fa16121694a954df442a1d7f86c04bed49e4
Jonas Devries <jonas@good-it.be>
2020-04-11 13:17:55 +0200

    add InternalViewResolver with config  - prefix /WEB-INF/views/  - suffix .jsp
     rearrange .jsp files
    
    rearrange spring context xml
    
    HomeController

---
+++ src/main/java/be/jslm/services/MembersService.java
@@ -0,0 +1,10 @@
+package be.jslm.services;
+
+import be.jslm.domain.Member;
+
+import java.util.List;
+
+public interface MembersService {
+
+    List<Member> getAllMembers();
+}
---
+++ src/main/java/be/jslm/services/MembersServiceImpl.java
@@ -0,0 +1,13 @@
+package be.jslm.services;
+
+import be.jslm.domain.Member;
+
+import java.util.List;
+
+public class MembersServiceImpl implements MembersService {
+
+    @Override
+    public List<Member> getAllMembers() {
+        return null;
+    }
+}
